42e482381e4d1f268023838fc259eedea4f59fdd
Svetlana Martianova <smartianova8@mail.ru>
2023-12-16 18:40:40 +0300

    fix:test

--- bfs.cpp
+++ bfs.cpp
@@ -1,31 +1,46 @@
-#include <iostream>
 #include <vector>
-#include <string>
+#include <stack>
+#include <iostream>
 #include <fstream>
-#include <sstream>
-#include <cstring>
 using namespace std;
 
-void bfs(vector<vector<int>> adjacencyMatrix){
+void bfs(vector<vector<int>> adjacencyMatrix, int v){
+    stack <int> vstak; 
     int sizeGraph=adjacencyMatrix.size();
     bool visited[sizeGraph];
     for (int i=0; i<sizeGraph; i++){
         visited[i]=false;
     }
-    cout<<visited[0];
+    vstak.push(v);
+    visited[v]=true;
+    while (vstak.size()!=0){
+        stack <int> stack;
+        while (vstak.size()>0){
+            int vert=vstak.top();
+            vector<int> listVert=adjacencyMatrix[vert];
+            for (int i=0; i<listVert.size(); i++){
+                if (listVert[i]==1 && !visited[i]){
+                    stack.push(i);
+                    visited[i]=true;
+                }
+            }
+            vstak.pop();
+        }
+        vstak=stack;
+    }
+
     
 }
 
 int main() {
     vector<vector<int>> adjacencyMatrix;
-    adjacencyMatrix.resize(5);
-
-    for (int i=0; i<5;i++){
-        adjacencyMatrix[i].resize(5);
-        for (int j=0; j<5; j++){
-            adjacencyMatrix[i][j]=0;
-        }
+    adjacencyMatrix.resize(6);
+    ifstream read("data.txt");
+    for (int i=0; i<6;i++){
+        adjacencyMatrix[i].resize(6);
+        read>>adjacencyMatrix[i][0]>>adjacencyMatrix[i][1]>>adjacencyMatrix[i][2]>>adjacencyMatrix[i][3]>>adjacencyMatrix[i][4]>>adjacencyMatrix[i][5];
+        
     }
-    bfs(adjacencyMatrix);
+    bfs(adjacencyMatrix, 0);
     return 0;
 }
