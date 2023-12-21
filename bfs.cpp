872d5d1f05c33a966d2c2fe8a4c0d61f567e48be
Svetlana Martianova <smartianova8@mail.ru>
2023-12-21 23:30:41 +0300

    bfs use in main.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -22,20 +22,8 @@ void bfs(vector<vector<int>> adjacencyMatrix, int v){
                     visited[i]=true;
                 }
             }
-            cout<<vqueue.front();
+            //cout<<vqueue.front()<<endl;
             vqueue.pop();
         }
 }
 
-int main() {
-    vector<vector<int>> adjacencyMatrix;
-    adjacencyMatrix.resize(6);
-    ifstream read("data.txt");
-    for (int i=0; i<6;i++){
-        adjacencyMatrix[i].resize(6);
-        read>>adjacencyMatrix[i][0]>>adjacencyMatrix[i][1]>>adjacencyMatrix[i][2]>>adjacencyMatrix[i][3]>>adjacencyMatrix[i][4]>>adjacencyMatrix[i][5];
-        
-    }
-    bfs(adjacencyMatrix, 0);
-    return 0;
-}
--- graph.h
+++ graph.h
@@ -10,6 +10,7 @@ public:
     Graph(int vertices);
     void addEdge(int v, int w); // setting 1's at places where the edge is present
     void displayMatrix(); // easy matrix display
+    std::vector<std::vector<int>> getAdjacencyMatrix();
 
 private:
     int vertices;
